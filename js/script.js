b603d9dde4b8ee8bbe64b8ee2577f382bef016a3
spectrefvm <rtfhffjfhtmimi@googlemail.com>
2025-03-03 23:47:43 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const deleteBtn = document.getElementById("delete");
 const delUrl = document.getElementById("delUrl");
 let interval;
 
-// Start Spamming
+
 start.addEventListener("click", async () => {
     if (!url.value || !content.value) {
         alert("Please fill out all required info!");
@@ -28,7 +28,6 @@ start.addEventListener("click", async () => {
     }
 });
 
-// Stop Spamming
 stop.addEventListener("click", async () => {
     clearInterval(interval);
     start.disabled = false;
@@ -36,7 +35,6 @@ stop.addEventListener("click", async () => {
     alert("Stopped spamming!");
 });
 
-// Sending message to the webhook
 async function send() {
     const payload = {
         username: username.value,
@@ -57,23 +55,25 @@ async function send() {
     }
 }
 
-// Deleting Webhook
+
+
 deleteBtn.addEventListener("click", async () => {
     if (!delUrl.value) {
-        alert("Please fill out all required info!");
-        return false;
+      alert("Please fill out all required info!");
+      return false;
     }
     try {
-        const res = await fetch(delUrl.value);
-        if (!res.ok) {
-            throw new Error(`Error: ${response.status} ${response.statusText}`);
-        }
-        del();
-        alert("Success, webhook deleted successfully!");
+      const res = await fetch(delUrl.value);
+      if (!res.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+      del();
+      alert("Success, webhook deleted successfully!");
     } catch (e) {
-        alert("Invalid webhook URL!");
+      alert("Invalid webhook URL!");
     }
 });
+
 
 async function del() {
     try {
@@ -87,11 +87,3 @@ async function del() {
         console.log(e);
     }
 }
-
-// Password protection logic
-const correctPassword = 'yourpassword'; // Replace with your actual password
-
-// Get elements for password protection
-const submitPasswordButton = document.getElementById('submitPassword');
-const passwordInput = document.getElementById('password');
-const passwordPrompt = document
